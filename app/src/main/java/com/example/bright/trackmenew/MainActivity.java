f9c2f148fee0397299e7f753242bc8bf0d60fac0
rishav <rishavsaigal@gmail.com>
2018-05-13 12:32:13 +0530

    Code done Except Firebase

--- app/src/main/java/com/example/bright/trackmenew/MainActivity.java
+++ app/src/main/java/com/example/bright/trackmenew/MainActivity.java
@@ -1,19 +1,94 @@
 package com.example.bright.trackmenew;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends AppCompatActivity
+        implements LocationListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleMap mMap;
+    private LatLng sydney;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+
+    @SuppressLint({"MissingPermission", "RestrictedApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        locationRequest = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
-    public void showMyLocation(View view) {
-        startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+    @SuppressLint({"MissingPermission", "RestrictedApi"})
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        sydney = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Your Current Location")
+                .draggable(false)
+                .flat(false)
+                .visible(true));
+        mMap.setMinZoomPreference(8);
+        mMap.setIndoorEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        googleApiClient.disconnect();
+    }
+
+    @SuppressLint({"RestrictedApi", "MissingPermission"})
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
     }
 }
